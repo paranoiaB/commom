095fa0dfe09a88f00085a1d4b9e6ed2b9f92cdeb
seanxywang(王星雅) <seanxywang@tencent.com>
2016-08-05 21:29:24 +0800

    add

--- func.cpp
+++ func.cpp
@@ -1,5 +1,5 @@
 #include "func.h"
-namespace commom{
+namespace com{
 	void LOG_INFO(string str){
 		std::cout<<"LOG_INFO :"<<str<<std::endl;
 	}
@@ -16,19 +16,19 @@ namespace commom{
 		string bufferstr = buffer;	
 		return bufferstr;	
 	}	
-	void Split(const char* d,const string& str, std::vector< string >& vec){
-		string  delim = d; 
+	void Split(const char* d,const string& str, vec_str& vec){
+		string delim = d; 
 		vec.clear();
 		size_t last = 0, index=str.find(delim,last);	
 		while (index!=string::npos){
 			vec.push_back(str.substr(last,index-last));	
 			last=index+delim.size();index=str.find(delim,last);
 		}	
-		if (index-last>0)	vec.push_back(str.substr(last,index-last));
+		if (index-last>0)vec.push_back(str.substr(last,index-last));
 	}
-	string  Unit(const string& str){
+	string Unit(const string& str){
 		string restr = "";
-		std::vector<string> words;
+		vec_str words;
 		Split(" ", str, words);
 		for(int jj =0; jj< words.size(); jj++){
 			if(words.at(jj) == " ")continue;
@@ -36,7 +36,7 @@ namespace commom{
 		}
 		return restr;
 	}
-	void  SplitOnce(const char* d,const string& str, std::vector< string >& vec){
+	void SplitOnce(const char* d,const string& str, vec_str& vec){
 		string delim = d;
 		vec.clear();
 		int index = str.find(delim);
@@ -48,10 +48,10 @@ namespace commom{
 		vec.push_back(temp);
 	}
 
-	void  _Split(const char* d,const string& str, std::vector< string >& vec){
+	void _Split(const char* d,const string& str, vec_str& vec){
 		string delim = d;
 		vec.clear();
-		std::map<string, int>temp;
+		map_sint temp;
 		size_t last = 0, index=str.find(delim,last);
 		while (index!=string::npos){	
 			temp[str.substr(last,index-last)]++;	
@@ -60,10 +60,10 @@ namespace commom{
 		}
 		if (index-last>0)	
 			temp[str.substr(last,index-last)]++;	
-		for(std::map<string, int>::iterator it = temp.begin(); it!= temp.end(); it++)
+		for(map_sint::iterator it = temp.begin();it!= temp.end(); it++)
 			vec.push_back(it->first);
 	}
-	void  Split(const char* d,const string& str, std::map<string, int>& vec){
+	void Split(const char* d,const string& str, map_sint& vec){
 		string delim = d;
 		vec.clear();
 		size_t last = 0,index=str.find(delim,last);
@@ -72,20 +72,20 @@ namespace commom{
 			last=index+delim.size();
 			index=str.find(delim,last);
 		}	
-		if (index-last>0)	vec[str.substr(last,index-last)]++;	
+		if (index-last>0)vec[str.substr(last,index-last)]++;	
 	}
 
-	uint16_t StrToInt(const string& str){
+	uint16 StrToInt(const string& str){
 		char leftstr, rightstr;	
 		if(str.empty())return 0;
 		int i =0;
-		if(!(str[i] & 0x80)) {
+		if(!(str[i] & 0x80)){
 			return str[i];
-		}	else if ((unsigned char)str[i] <= 0xdf && i + 1 < str.size()) {
+		}else if((unsigned char)str[i] <= 0xdf && i+1<str.size()){
 			leftstr = (str[i] >> 2) & 0x07;	
-			rightstr = (str[i+1] & 0x3f) | ((str[i] & 0x03) << 6 );	
+			rightstr = (str[i+1] & 0x3f) | ((str[i] & 0x03) << 6);	
 			return Combine(leftstr, rightstr);
-		}	else if ((unsigned char)str[i] <= 0xef && i + 2 < str.size()) {	
+		}else if((unsigned char)str[i] <= 0xef && i + 2 < str.size()){	
 			leftstr = (str[i] << 4) | ((str[i+1] >> 2) & 0x0f );	
 			rightstr = ((str[i+1]<<6) & 0xc0) | (str[i+2] & 0x3f); 
 			return Combine(leftstr, rightstr);	
@@ -94,26 +94,26 @@ namespace commom{
 		}
 
 	}
-	bool StrToVec(const string& str,  intvector& v){
+	bool StrToVec(const string& str, intvector& v){
 		char leftstr, rightstr;	
 		if(str.empty())return false;
 		v.clear();
 		for( unsigned int i = 0; i <str.size(); ){
 			if(!(str[i] & 0x80)) {
 				v.push_back(str[i++]);
-			}	else if ((unsigned char)str[i] <= 0xdf && i + 1 < str.size()) {
+			}else if ((unsigned char)str[i] <= 0xdf && i + 1 < str.size()) {
 				leftstr = (str[i] >> 2) & 0x07;	
 				rightstr = (str[i+1] & 0x3f) | ((str[i] & 0x03) << 6 );	
 				v.push_back(Combine(leftstr, rightstr));	i += 2;
-			}	else if ((unsigned char)str[i] <= 0xef && i + 2 < str.size()) {	
+			}else if ((unsigned char)str[i] <= 0xef && i + 2 < str.size()) {	
 				leftstr = (str[i] << 4) | ((str[i+1] >> 2) & 0x0f );	
 				rightstr = ((str[i+1]<<6) & 0xc0) | (str[i+2] & 0x3f); 
 				v.push_back(Combine(leftstr, rightstr));	i += 3;	
-			} else if((unsigned char)str[i] <= 0xf7 ){
+			}else if((unsigned char)str[i] <= 0xf7 ){
 				i += 4;
-			} else if((unsigned char)str[i] <= 0xfb ){
+			}else if((unsigned char)str[i] <= 0xfb ){
 				i += 5;
-			} else if((unsigned char)str[i] <= 0xfd ){
+			}else if((unsigned char)str[i] <= 0xfd ){
 				i += 6;
 			}else{
 				return false;
@@ -129,7 +129,7 @@ namespace commom{
 			else if( v[i] <= 0x7ff ){
 				str += char((( v[i]>>6) & 0x1f) | 0xc0);	
 				str += char(( v[i] & 0x3f) | 0x80);
-			} else{
+			}else{
 				str += char((( v[i] >> 12) & 0x0f )| 0xe0);
 				str += char((( v[i]>>6) & 0x3f )| 0x80 );	
 				str += char(( v[i] & 0x3f) | 0x80);
@@ -138,92 +138,8 @@ namespace commom{
 		}
 		return str;
 	}
-
-	void Brackets(string& str){
-		//【】[]{}
-		//commom::DEBUG_INFO(str);
-		string strcopy = "";
-		string::size_type indexa = str.find("【");
-		string::size_type indexad = 0;
-		if(indexa != string::npos){
-			indexad =  str.find("】");
-			if((indexad != string::npos)&&(indexad > indexa)){
-				strcopy.append(str,0,indexa);
-				strcopy.append(str,indexad -1, str.size()-1);
-			}else{
-				strcopy = str;
-			}
-		}else{
-			strcopy = str;
-		}
-		str = strcopy;
-		strcopy = "";
-		indexa = str.find("{");
-		if(indexa != string::npos){
-			indexad =  str.find("}");
-			if((indexad != string::npos)&&(indexad > indexa)){
-				strcopy.append(str,0,indexa);
-				strcopy.append(str,indexad + 1, str.size()-1);
-			}else{
-				strcopy = str;
-			}
-		}else{
-			strcopy = str;
-		}
-		str = strcopy;
-		strcopy = "";
-		indexa = str.find("[");
-		if(indexa != string::npos){
-			indexad =  str.find("]");
-			if((indexad != string::npos)&&(indexad > indexa)){
-				strcopy.append(str,0,indexa);
-				strcopy.append(str,indexad + 1, str.size()-1);
-			}else{
-				strcopy = str;
-			}
-		}else{
-			strcopy = str;
-		}
-		strcopy = "";
-		indexa = str.find("(");
-		if(indexa != string::npos){
-			//commom::DEBUG_INFO("find [");
-			indexad =  str.find(")");
-			if((indexad != string::npos)&&(indexad > indexa)){
-				//commom::DEBUG_INFO("find ]");
-				strcopy.append(str,0,indexa);
-				//commom::DEBUG_INFO(strcopy);
-				strcopy.append(str,indexad + 1, str.size()-1);
-				//commom::DEBUG_INFO(strcopy);
-			}else{
-				strcopy = str;
-			}
-		}else{
-			strcopy = str;
-		}
-		str = strcopy;
-		strcopy = "";
-		indexa = str.find("（");
-		if(indexa != string::npos){
-			//commom::DEBUG_INFO("find [");
-			indexad =  str.find("）");
-			if((indexad != string::npos)&&(indexad > indexa)){
-				//commom::DEBUG_INFO("find ]");
-				strcopy.append(str,0,indexa);
-				//commom::DEBUG_INFO(strcopy);
-				strcopy.append(str,indexad + 1, str.size() - indexad );
-				//commom::DEBUG_INFO(strcopy);
-			}else{
-				strcopy = str;
-			}
-		}else{
-			strcopy = str;
-		}
-		str = strcopy;
-	}
-
+	
 	string MapToJason(std::map<string,string>& dcit){
-		//{'startDate': '2015-01-01 10:00:00',
 		string str = "";
 		str += "{";
 		for(std::map<string,string>::iterator it = dcit.begin(); it != dcit.end(); it++){
@@ -286,26 +202,25 @@ namespace commom{
 		return timestr;
 	}
 
-
-
-	char* ReadLine( char* buffer, int  size, FILE *fi ){
+	//文件处理
+	char* ReadLine( char* buffer, int size, FILE *fi ){
 		memset( buffer,0,size );
 		return fgets(buffer, size, fi);
 	}
 	int WiteLine( const char *str, FILE *fo ){
 		return fputs(str,fo);
 	}
-	bool GetDirfile(std::vector<string>& filelist, const char* path){   
-		DIR *dp;    
-		struct dirent *dirp;    
+	bool GetDirfile(vec_str& filelist, const char* path){ 
+		DIR *dp; 
+		struct dirent *dirp; 
 		if((dp=opendir(path))==NULL){
-			commom::DEBUG_INFO("open dir error");
+			com::DEBUG_INFO("open dir error");
 		}
-		while ((dirp=readdir(dp))!=NULL){   
-			//commom::DEBUG_INFO(dirp->d_name);    	
+		while ((dirp=readdir(dp))!=NULL){ 
+			//com::DEBUG_INFO(dirp->d_name); 	
 			filelist.push_back(dirp->d_name);
 		}
-		closedir(dp);    
+		closedir(dp); 
 		delete(dirp);
 		return true;
 	}	
--- func.h
+++ func.h
@@ -1,74 +1,34 @@
 #ifndef _FUNC_H
 #define _FUNC_H
-#include <stdio.h>
-#include <iostream>
-#include <string>
-#include "string.h"
-#include <stdlib.h>
-#include <stdint.h>
-#include <sys/types.h>    
-#include <sys/stat.h>
-#include <vector>
-#include <sstream>
-#include <map>
-#include <dirent.h>  
-#include <time.h>
-#define SHORT_LENTH 250
-#define MAX_LENTH 50000
-typedef FILE* _FILE;
-typedef std::string string;
-typedef struct{ string word; float d;}wdlen;
-typedef  std::vector<uint16_t>  intvector;
-namespace commom{
-	//日志类
-	void LOG_INFO(string str);
-	void DEBUG_INFO(string str);
+#include "header.h"
+namespace com{
+	void LOG_INFO ( string str);
+	void DEBUG_INFO( string str);
 
-	//字符串处理
-	string GetLine( char* buffer );
-	string  Unit(const string& str);
-	void Split(const char* d,const string& str, std::vector< string >& vec);
-	void Split(const char* d,const string& str, std::map<string, int>& vec);
-	void _Split(const char* d,const string& str, std::vector< string >& vec);
-	void SplitOnce(const char* d,const string& str, std::vector< string >& vec);			
-	inline uint16_t Combine(char high, char low){	return (((uint16_t(high) & 0x00ff ) << 8) | (uint16_t(low) & 0x00ff));	}		
-	bool StrToVec(const string& str,  intvector& v);
-	uint16_t  StrToInt(const string& str);
-	string  VecToStr(intvector& v, int i,int j );
+	string GetLine(char* buffer );
+	string Unit(const string& str);
+	void  Split(const char* d, const string& str, vec_str& vec);
+	void  Split(const char* d, const string& str, map_sint& vec);
+	void  _Split(const char* d,const string& str, vec_str& vec);
+	void  SplitOnce(const char* d,const string& str, vec_str& vec);			
+	bool  StrToVec(const string& str, intvector& v);
+	uint16 StrToInt(const string& str);
+	string VecToStr(intvector& v, int i,int j );
 	string MapToJason(std::map<string,string>& dcit);
-	//去括号里内容
-	void Brackets(string& str);
-	//文件处理
-	char* ReadLine( char* buffer, int  size, FILE *fi );
-	int WiteLine( const char *str, FILE *fo );
-	bool GetDirfile(std::vector<string>& filelist, const char* path);
-	//时间处理
+	inline uint16 Combine(char high, char low){
+		return (((uint16(high)&0x00ff)<<8)|(uint16(low)&0x00ff));	
+	}		
+	char*	ReadLine(char* buffer, int size, FILE *fi);
+	int		WiteLine(const char *str, FILE *fo);
+	bool	GetDirfile(vec_str& filelist, const char* path);
+	
 	string GetTime();
 	string GetDate();
 
-	//常用模版
-	template <class T> int MaxArray(T* prob, int n){
-		if((n < 1 )||(prob == NULL))return -1;
-		T temp = prob[0];	int index =0;
-		for(int i =0; i< n; i++){
-			if(temp < prob[i] ){
-				temp = prob[i]; 	index = i;
-			}
-		}
-		return index;
-	}
-	template <class T> int MinArray(T* prob, int n){
-		if((n < 1 )||(prob == NULL))return -1;
-		T temp = prob[0];	int index =0;
-		for(int i =0; i< n; i++){
-			if(temp > prob[i] ){
-				temp = prob[i]; 	index = i;
-			}
-		}
-		return index;
-	}
 	template <class T> string ConvertToStr(T value) {
-		std::stringstream ss;	ss << value;	return ss.str();
+		std::stringstream ss;	
+		ss << value;	
+		return ss.str();
 	}
 	static bool SortByDistance(const wdlen& x, const wdlen& y){
 		return x.d > y.d;
@@ -81,67 +41,29 @@ namespace commom{
 		const std::pair<string, float>& y ){
 			return x.second < y.second;
 	}
-	/*
-	class Func{
-	public://字符串处理
-		string GetLine( char* buffer );
-		string  Unit(const string& str);
-		void Split(const char* d,const string& str, std::vector< string >& vec);
-		void Split(const char* d,const string& str, std::map<string, int>& vec);
-		void _Split(const char* d,const string& str, std::vector< string >& vec);
-		void SplitOnce(const char* d,const string& str, std::vector< string >& vec);			
-		inline uint16_t Combine(char high, char low){	return (((uint16_t(high) & 0x00ff ) << 8) | (uint16_t(low) & 0x00ff));	}		
-		bool StrToVec(const string& str,  intvector& v);
-		uint16_t  StrToInt(const string& str);
-		string  VecToStr(intvector& v, int i,int j );
-		string MapToJason(std::map<string,string>& dcit);
-		//去括号里内容
-		void Brackets(string& str);
-	public://文件处理
-		char* ReadLine( char* buffer, int  size, FILE *fi );
-		int WiteLine( const char *str, FILE *fo );
-		bool GetDirfile(std::map<string,int>& filelist, const char* path);
-	public://时间处理
-		string GetTime();
-		string GetDate();
+	template <class T> int MaxArray(T* prob, int n){
+		if((n < 1 )||(prob == NULL))return -1;
+		T temp = prob[0];	int index =0;
+		for(int i =0; i< n; i++){
+			if(temp < prob[i] ){
+				temp = prob[i]; 	
+				index = i;
+			}
+		}
+		return index;
+	}
+	template <class T> int MinArray(T* prob, int n){
+		if((n < 1 )||(prob == NULL))return -1;
+		T temp = prob[0];	int index =0;
+		for(int i =0; i< n; i++){
+			if(temp > prob[i] ){
+				temp = prob[i]; 	
+				index = i;
+			}
+		}
+		return index;
+	}
 
-	public://常用模版
-		template <class T> int MaxArray(T* prob, int n){
-			if((n < 1 )||(prob == NULL))return -1;
-			T temp = prob[0];	int index =0;
-			for(int i =0; i< n; i++){
-				if(temp < prob[i] ){
-					temp = prob[i]; 	index = i;
-				}
-			}
-			return index;
-		}
-		template <class T> int MinArray(T* prob, int n){
-			if((n < 1 )||(prob == NULL))return -1;
-			T temp = prob[0];	int index =0;
-			for(int i =0; i< n; i++){
-				if(temp > prob[i] ){
-					temp = prob[i]; 	index = i;
-				}
-			}
-			return index;
-		}
-		template <class T> string ConvertToStr(T value) {
-			std::stringstream ss;	ss << value;	return ss.str();
-		}
-		static bool SortByDistance(const wdlen& x, const wdlen& y){
-			return x.d > y.d;
-		}
-		static bool SortBySecondGreater(const std::pair<string, float>& x, 
-			const std::pair<string, float>& y ){
-				return x.second > y.second;
-		}
-		static bool SortBySecondLess(const std::pair<string, float>& x, 
-			const std::pair<string, float>& y ){
-				return x.second < y.second;
-		}
-	};
-	*/
 
 }
 #endif
